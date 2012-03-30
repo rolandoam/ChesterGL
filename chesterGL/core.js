351503ad6e60836c95d1f4c3f9ff617e1fade3c6
Rolando Abarca <m@rolando.cl>
2012-03-29 20:06:44 -0700

    adds support for NPOT textures

--- chesterGL/core.js
+++ chesterGL/core.js
@@ -648,8 +648,10 @@ chesterGL.prepareWebGLTexture = function (texture) {
 			console.log("gl error " + error);
 			result = false;
 		}
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+		// gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 		gl.bindTexture(gl.TEXTURE_2D, null);
 	} catch (e) {
 		console.log("got some error: " + e);
