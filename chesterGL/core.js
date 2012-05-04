92b9814786d11a73d7bf293a93de49a16dc498c4
Rolando Abarca <m@rolando.cl>
2012-05-04 14:38:47 -0700

    small changes to the requestAnimationFrame

--- chesterGL/core.js
+++ chesterGL/core.js
@@ -41,7 +41,7 @@ HTMLCanvasElement._canvas_tmp_mouse = new goog.math.Vec2(0, 0);
 HTMLCanvasElement.prototype.relativePosition = function (evt) {
 	var pt = HTMLCanvasElement._canvas_tmp_mouse;
 	pt.x = 0; pt.y = 0;
-	if (this.__offset == undefined) {
+	if (this.__offset === undefined) {
 		this.__offset = $(this).offset();
 		this.__height = $(this).height();
 	}
@@ -51,21 +51,33 @@ HTMLCanvasElement.prototype.relativePosition = function (evt) {
 	return pt;
 };
 
-/**
- * @ignore
- * @type {function(function(), Element)}
- */
-window.requestAnimFrame = (function(){
-	return  window.requestAnimationFrame       ||
-			window.webkitRequestAnimationFrame ||
-			window.mozRequestAnimationFrame    ||
-			window.oRequestAnimationFrame      ||
-			window.msRequestAnimationFrame     ||
-			function(/* function */ callback, /* DOMElement */ element){
-				window.setTimeout(callback, 1000 / 60);
-			};
-})();
-goog.exportSymbol('requestAnimationFrame', window.requestAnimationFrame);
+// requestAnimationFrame polyfill by Erik Möller
+// fixes from Paul Irish and Tino Zijdel
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame) {
+		console.log("using setTimeout");
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = Date.now();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    }
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
 
 /**
  * @define {boolean}
@@ -1048,7 +1060,7 @@ chesterGL.removeMouseHandler = function (callback) {
 chesterGL.run = function () {
 	if (!chesterGL._paused) {
 		// console.time("mainLoop");
-		window.requestAnimFrame(chesterGL.run, chesterGL.canvas);
+		window.requestAnimationFrame(chesterGL.run, chesterGL.canvas);
 		chesterGL.drawScene();
 		chesterGL.ActionManager.tick(chesterGL.delta);
 		// console.timeEnd("mainLoop");
