35d7f88bb9ce54ebe106f9818d57094d3f898d01
Rolando Abarca <m@rolando.cl>
2012-09-08 19:54:47 -0700

    changes the mag_filter for textures (GL_NEAREST)

--- chesterGL/core.js
+++ chesterGL/core.js
@@ -705,7 +705,7 @@ chesterGL.prepareWebGLTexture = function (texture) {
 			console.log("gl error " + error);
 			result = false;
 		}
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
